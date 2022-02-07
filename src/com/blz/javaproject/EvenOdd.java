312722c8303420343a7048e86ebedca9b03cb4e4
erakshaykumar <akaakshay304@gmail.com>
2022-02-07 09:47:14 +0530

    added: Check Even or Odd

--- src/com/blz/javaproject/EvenOdd.java
+++ src/com/blz/javaproject/EvenOdd.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class EvenOdd {
 
 	public static void main(String[] args) {
-		  Scanner reader = new Scanner(System.in);
-
-	        System.out.print("Enter a number: ");
+		  try (Scanner reader = new Scanner(System.in)) {
+			System.out.print("Enter a number: ");
 	        int number = reader.nextInt();
 
 	        if(number % 2 == 0)
 	            System.out.println(number + " is even");
 	        else
 	            System.out.println(number + " is odd");
-	    }
+		}
 	}
+}
 
 
